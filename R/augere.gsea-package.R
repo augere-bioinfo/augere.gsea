20138c65823e245648433d492f2636704d7d550c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-21 17:33:51 +1100

    Assorted fixes in preparation for BioC review.
    Also added a bunch of biocViews.

---
+++ R/augere.gsea-package.R
@@ -0,0 +1,8 @@
+#' augere.gsea
+#'
+#' Implement gene set enrichment analyses in the augere framework.
+#' Pipeline functions execute gene set enrichment analyses with packages like limma, fgsea or goseq,
+#' also generating a self-contained Rmarkdown report to reproduce each analysis.
+#'
+#' @name augere.gsea-package
+NULL
